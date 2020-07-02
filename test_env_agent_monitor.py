82c582d62592fec846495821f72cead2558ff7cb
Sergei Volodin <sergei.volodin@epfl.ch>
2020-07-02 14:36:38 -0700

    Add tests for rps_rl.py

---
+++ test_env_agent_monitor.py
@@ -0,0 +1,15 @@
+import rock_paper_scissors as rps
+from rps_rl import RPSEnv, RPSAgent, Monitor, Universe
+
+def test_create_env():
+    env = RPSEnv(noise_dim=4)
+
+def test_create_agent():
+    env = RPSEnv(noise_dim=4)
+    agent = RPSAgent(noise_dim=env.noise_dim, identity=0)
+
+def test_create_monitor():
+    env = RPSEnv(noise_dim=4)
+    AGENTS = [RPSAgent(noise_dim=env.noise_dim, identity=i)
+              for i in range(5)]
+    m = Monitor(agents=AGENTS)
