a05c233e560cd30fadbea718ba32df463acc2607
Sergei Volodin <sergei.volodin@epfl.ch>
2020-08-10 19:28:03 +0200

    Re-format the code

--- youshallnotpass_rllib_adversarial.py
+++ youshallnotpass_rllib_adversarial.py
@@ -6,7 +6,6 @@ import subprocess
 import sys
 import argparse
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
 import numpy as np
 import ray
 from ray.rllib import agents
@@ -30,7 +29,7 @@ import gym
 from gym_compete_to_rllib import created_envs, env_name, create_env, env_name_rllib
 
 import os
-#os.environ['DISPLAY'] = ':0'
+# os.environ['DISPLAY'] = ':0'
 import codecs
 import time
 
@@ -39,14 +38,17 @@ from ray.tune.schedulers import ASHAScheduler
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
-#ex = Experiment("youshallnotpass_learned_adversary_vs_zoo", interactive=True)
-#ex.observers.append(MongoObserver(url='127.0.0.1:27017',
+tf.compat.v1.enable_eager_execution()
+
+# ex = Experiment("youshallnotpass_learned_adversary_vs_zoo", interactive=True)
+# ex.observers.append(MongoObserver(url='127.0.0.1:27017',
 #                                      db_name='better_adversarial_defenses'))
 
 env_cls = create_env
 env_config = {'with_video': False}
 
-#@ex.capture
+
+# @ex.capture
 def log_dict(d, prefix='', counter=0, _run=None):
     """Ray dictionary results to sacred."""
     for k, v in d.items():
@@ -54,6 +56,7 @@ def log_dict(d, prefix='', counter=0, _run=None):
             _run.log_scalar(k, v, counter)
         elif isinstance(v, dict):
             log_dict(d=d, prefix=k + '.', counter=counter)
+
 
 def ray_init(num_cpus=60, shutdown=True):
     """Initialize ray."""
@@ -63,10 +66,11 @@ def ray_init(num_cpus=60, shutdown=True):
     if not shutdown:
         kwargs['ignore_reinit_error'] = True
     return ray.init(num_cpus=num_cpus * 2, log_to_driver=False,
-            temp_dir='/scratch/sergei/tmp', resources={'tune_cpu': num_cpus,}, **kwargs)
+                    temp_dir='/scratch/sergei/tmp', resources={'tune_cpu': num_cpus, }, **kwargs)
 
 
-def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, num_workers_tf=32, env_config=env_config, load_normal=False):
+def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, num_workers_tf=32, env_config=env_config,
+                         load_normal=False):
     """Build configuration for 1 run."""
     obs_space = env_cls(env_config).observation_space
     act_space = env_cls(env_config).action_space
@@ -76,31 +80,31 @@ def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, nu
     def get_agent_config(agent_id):
         agent_config_pretrained = (PPOTFPolicy, obs_space, act_space, {
             'model': {
-                        "custom_model": "GymCompetePretrainedModel",
-                        "custom_model_config": {
-                            "agent_id": agent_id - 1,
-                            "env_name": env_name,
-                            "model_config": {},
-                            "name": "model_%s" % (agent_id - 1)
-                        },           
-                        
-                    },
-            
+                "custom_model": "GymCompetePretrainedModel",
+                "custom_model_config": {
+                    "agent_id": agent_id - 1,
+                    "env_name": env_name,
+                    "model_config": {},
+                    "name": "model_%s" % (agent_id - 1)
+                },
+
+            },
+
             "framework": "tfe",
         })
-        
+
         agent_config_from_scratch = (PPOTFPolicy, obs_space, act_space, {
-                    "model": {
-                        "use_lstm": False,
-                        "fcnet_hiddens": [64, 64],
-                        #"custom_action_dist": "DiagGaussian",
-                        "fcnet_activation": "tanh",
-                        "free_log_std": True,
-                    },
-                    "framework": "tfe",
-                    "observation_filter": "MeanStdFilter",
-                })
-        
+            "model": {
+                "use_lstm": False,
+                "fcnet_hiddens": [64, 64],
+                # "custom_action_dist": "DiagGaussian",
+                "fcnet_activation": "tanh",
+                "free_log_std": True,
+            },
+            "framework": "tfe",
+            "observation_filter": "MeanStdFilter",
+        })
+
         if agent_id == 1:
             return agent_config_pretrained if load_normal else agent_config_from_scratch
         elif agent_id == 2:
@@ -110,22 +114,22 @@ def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, nu
 
     N_POLICIES = 2
 
-    policies = {policy_template % i: get_agent_config(i) for i in range(1, 1  + N_POLICIES)}
+    policies = {policy_template % i: get_agent_config(i) for i in range(1, 1 + N_POLICIES)}
     policies_keys = list(sorted(policies.keys()))
 
     def select_policy(agent_id):
         assert agent_id in ["player_1", "player_2"]
         agent_ids = ["player_1", "player_2"]
-        
+
         # selecting the corresponding policy (only for 2 policies)
         return policies_keys[agent_ids.index(agent_id)]
 
         # randomly choosing an opponent
         # return np.random.choice(list(policies.keys()))
-    
+
     if train_policies is None:
         train_policies = list(policies.keys())
-        
+
     for k in train_policies:
         assert k in policies.keys()
 
@@ -149,15 +153,15 @@ def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, nu
         "gamma": 0.995,
         "sgd_minibatch_size": int(config.get("sgd_minibatch_size", 128)),
         "num_sgd_iter": int(config.get("num_sgd_iter", 30)),
-        
+
         'tf_session_args': {'intra_op_parallelism_threads': num_workers_tf,
-          'inter_op_parallelism_threads': num_workers_tf,
-          'gpu_options': {'allow_growth': True},
-          'log_device_placement': True,
-          'device_count': {'CPU': num_workers_tf},
-          'allow_soft_placement': True
-        },
-        
+                            'inter_op_parallelism_threads': num_workers_tf,
+                            'gpu_options': {'allow_growth': True},
+                            'log_device_placement': True,
+                            'device_count': {'CPU': num_workers_tf},
+                            'allow_soft_placement': True
+                            },
+
         "local_tf_session_args": {
             "intra_op_parallelism_threads": num_workers_tf,
             "inter_op_parallelism_threads": num_workers_tf,
@@ -166,16 +170,17 @@ def build_trainer_config(train_policies, config, num_workers=4, use_gpu=True, nu
     }
     return config
 
+
 def train_iteration_process(pickle_path, ray_init=True):
     print("Load pickle")
     checkpoint, config = pickle.load(open(pickle_path, 'rb'))
     print("Ray init")
     if ray_init:
         ray.init(address=config['redis_address'])
-    #ray.init(num_cpus=5, include_dashboard=False)
+    # ray.init(num_cpus=5, include_dashboard=False)
     print("Build config")
     rl_config = build_trainer_config(train_policies=config['train_policies'],
-                              config=config)
+                                     config=config)
     print("Create trainer")
     trainer = PPOTrainer(config=rl_config)
     print("Restore")
@@ -191,8 +196,9 @@ def train_iteration_process(pickle_path, ray_init=True):
     print("Dump")
     trainer.stop()
     pickle.dump(results, open(pickle_path + '.ans.pkl', 'wb'))
-    #ray.shutdown()
+    # ray.shutdown()
     print("Done")
+
 
 def train_one(config, checkpoint=None, do_track=True):
     print("Building trainer config...")
@@ -201,16 +207,18 @@ def train_one(config, checkpoint=None, do_track=True):
     if not isinstance(checkpoint, str):
         checkpoint = None
     restore_state = None
-    
+
     print("Starting iterations...")
-   
+
     def train_iteration(checkpoint, config):
         pickle_path = '/tmp/' + str(uuid.uuid1()) + '.pkl'
         pickle_path_ans = pickle_path + '.ans.pkl'
         pickle.dump([checkpoint, config], open(pickle_path, 'wb'))
         # overhead doesn't seem significant!
-        subprocess.run("python %s --one_trial %s 2>&1 > %s" % (config['main_filename'], pickle_path, pickle_path + '.err'), shell=True)
-        #train_iteration_process(pickle_path, ray_init=False)
+        subprocess.run(
+            "python %s --one_trial %s 2>&1 > %s" % (config['main_filename'], pickle_path, pickle_path + '.err'),
+            shell=True)
+        # train_iteration_process(pickle_path, ray_init=False)
         os.unlink(pickle_path)
         try:
             results = pickle.load(open(pickle_path_ans, 'rb'))
@@ -236,6 +244,7 @@ def train_one(config, checkpoint=None, do_track=True):
         if iteration > config['train_steps']:
             return
 
+
 def get_config_coarse():
     # try changing learning rate
     config = {}
@@ -251,6 +260,7 @@ def get_config_coarse():
     config['train_policies'] = ['player_1']
     return config
 
+
 def get_config_fine():
     # try changing learning rate
     config = {}
@@ -265,6 +275,7 @@ def get_config_fine():
     # ['humanoid_blocker', 'humanoid'],
     config['train_policies'] = ['player_1']
     return config
+
 
 def get_config_small():
     # try changing learning rate
@@ -299,35 +310,34 @@ def main(_run=None):
     config['main_filename'] = sys.argv[0]
     config['redis_address'] = cluster_info['redis_address']
 
-    #config = {}
-    #config['train_batch_size'] = 320000
-    #config['lr'] = 3e-4
-    #config['num_sgd_iter'] = 20
-    #config['sgd_minibatch_size'] = 32768
-    #config['train_policies'] = ['player_1']
-    #config['train_steps'] = 10000
+    # config = {}
+    # config['train_batch_size'] = 320000
+    # config['lr'] = 3e-4
+    # config['num_sgd_iter'] = 20
+    # config['sgd_minibatch_size'] = 32768
+    # config['train_policies'] = ['player_1']
+    # config['train_steps'] = 10000
 
-    #train_one(config=config, do_track=False)
-    #return
+    # train_one(config=config, do_track=False)
+    # return
 
     analysis = tune.run(
-            train_one, 
-            config=config, 
-            verbose=True,
-            name="adversarial_tune_fine",
-            num_samples=300,
-            checkpoint_freq=0, # checkpoints done by the function itself
-            #scheduler=custom_scheduler,
-            resources_per_trial={"custom_resources": {"tune_cpu": 4}},
-            queue_trials=True,
-            #resume=True,
-            stop={'timesteps_total': 50000000} # 30 million time-steps
-        )
+        train_one,
+        config=config,
+        verbose=True,
+        name="adversarial_tune_fine",
+        num_samples=300,
+        checkpoint_freq=0,  # checkpoints done by the function itself
+        # scheduler=custom_scheduler,
+        resources_per_trial={"custom_resources": {"tune_cpu": 4}},
+        queue_trials=True,
+        # resume=True,
+        stop={'timesteps_total': 50000000}  # 30 million time-steps
+    )
 
 
 parser = argparse.ArgumentParser(description='Train in YouShallNotPass')
 parser.add_argument('--one_trial', type=str, help='Trial to run (if None, run tune)', default=None, required=False)
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
