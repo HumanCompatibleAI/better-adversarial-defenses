5bccd850bb64c791f38cc123d1b71ca97a709d4e
Sergei Volodin <sergei.volodin@epfl.ch>
2020-08-10 19:26:24 +0200

    Add script to compare rllib/gym env speed

--- env_profile.py
+++ env_profile.py
@@ -1,3 +1,17 @@
+import gym
+import gym_compete
+from tqdm import tqdm
+import numpy as np
+env_name = 'multicomp/YouShallNotPassHumans-v0'
+env = gym.make(env_name)
+obs = env.reset()
+for _ in tqdm(range(10000)):
+    act = (np.zeros(17), np.zeros(17))
+    obs, rew, done, info = env.step(act)
+    if done:
+        env.reset()
+
+
 from gym_compete_to_rllib import create_env
 from tqdm import tqdm
 import numpy as np
@@ -5,7 +19,7 @@ import numpy as np
 config = {'with_video': False}#False}
 env = create_env(config=config)
 obs = env.reset()
-for _ in tqdm(range(50000000)):
+for _ in tqdm(range(10000)):
     action_dict = {'player_1': np.random.randn(17), 'player_2': np.random.randn(17)}
     obs, rew, done, info = env.step(action_dict)
     if done['__all__']:
