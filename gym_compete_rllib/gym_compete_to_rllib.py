0ff200a4b30914d4768cd9eee2fb8d8da7bf0c3f
Sergei Volodin <sergei.volodin@epfl.ch>
2020-08-19 00:34:00 +0200

    Add single-agent version of the env with an agent embedded into it

--- gym_compete_rllib/gym_compete_to_rllib.py
+++ gym_compete_rllib/gym_compete_to_rllib.py
@@ -18,6 +18,66 @@ def dct_to_float32(d):
     return {x: np.array(y, dtype=np.float32) for x, y in d.items()}
 
 
+def model_to_callable(m):
+    """Convert a Keras model to a callable for a single object."""
+    def f(x):
+        return m.predict(np.array([x]))[0]
+    return f
+
+
+class MultiAgentToSingleAgent(gym.Env):
+    """Embed an agent into a multi-agent environment."""
+    def __init__(self, env_config):
+        self._env = env_config['env']
+        self.action_space = self._env.action_space
+        self.observation_space = self._env.observation_space
+        self.policies = env_config['policies']
+        self.players = []
+        self.previous_obs = {}
+        
+    @staticmethod
+    def dict_single_value(d):
+        """Get single value in a dict."""
+        assert len(d) == 1, f"Must have only 1 key, got {d.keys()}"
+        return list(d.values())[0]
+    
+    def other_player_value(self, d, check_len=True, default=None):
+        """Get value for the remaining player."""
+        if check_len and default is None:
+            assert len(d) - len(self.policies) == 1, f"Must have one value remaining. {d}"
+        for k, v in d.items():
+            if k not in self.policies:
+                return v
+        return default
+        
+    def render(self, *args, **kwargs):
+        return self._env.render(*args, **kwargs)
+        
+    def reset(self):
+        obs = self._env.reset()
+        self.players = list(obs.keys())
+        self.previous_obs = obs
+        return self.other_player_value(obs)
+    
+    def step(self, action):
+        actions = {}
+        for p in self.players:
+            if p in self.policies:
+                policy = self.policies[p]
+                actions[p] = policy(self.previous_obs[p])
+            else:
+                actions[p] = action
+                
+        obss, rews, dones, infos = self._env.step(actions)
+        if obss:
+            self.previous_obs = obss
+        else:
+            obss = self.previous_obs
+                
+        return self.other_player_value(obss), self.other_player_value(rews, default=np.array(0, dtype=np.float32)),\
+               self.other_player_value(dones, check_len=False), self.other_player_value(infos, check_len=False)
+
+
 class GymCompeteToRLLibAdapter(MultiAgentEnv):
     """Takes gym_compete env and makes a multi-agent RLLib env."""
 
@@ -97,7 +157,7 @@ class GymCompeteToRLLibAdapter(MultiAgentEnv):
         return self._env.action_space[0]
 
     def render(self, *args, **kwargs):
-        self._env.render(*args, **kwargs)
+        return self._env.render(*args, **kwargs)
 
 
 class KerasModelModel(TFModelV2):
