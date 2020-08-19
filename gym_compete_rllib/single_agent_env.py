1bd09a4544c8b66e8c8fce468b008adacca77cac
Sergei Volodin <sergei.volodin@epfl.ch>
2020-08-19 20:25:34 +0200

    Tf.agents code with 1 env

--- gym_compete_rllib/single_agent_env.py
+++ gym_compete_rllib/single_agent_env.py
@@ -1,6 +1,7 @@
 from gym.envs.registration import register
 from functools import partial
 import ray
+import numpy as np
 
 ray.init()
 env_name = 'multicomp/YouShallNotPassHumans-v0'
@@ -11,7 +12,6 @@ class policy_actor(object):
     def __init__(self):
         from gym_compete_rllib.load_gym_compete_policy import get_policy_value_nets
         from gym_compete_rllib.gym_compete_to_rllib import model_to_callable
-        self.c = model_to_callable(get_policy_value_nets(env_name, 1)['policy'])
         
     def call(self, *args):
         return self.c(*args)
@@ -19,6 +19,12 @@ class policy_actor(object):
     
 #pool = ray.util.ActorPool([policy_actor.remote() for _ in range(10)])
 p = policy_actor.remote()
+
+from gym_compete_rllib.load_gym_compete_policy import get_policy_value_nets
+from gym_compete_rllib.gym_compete_to_rllib import model_to_callable
+
+policy_model_1 = lambda o: np.zeros(17)
+#policy_model_1 = model_to_callable(get_policy_value_nets(env_name, 1)['policy'])
     
 def create_env_embed_agent(env_name):
     """Create a single-agent env."""
@@ -33,13 +39,13 @@ def create_env_embed_agent(env_name):
     from gym_compete_rllib.gym_compete_to_rllib import model_to_callable
     import gym
     
+    #policy_model_1 = lambda o, pool=pool: list(pool.map(lambda a, v: a.call.remote(v), [o]))[0]
+    policy_model_1 = model_to_callable(get_policy_value_nets(env_name, 1)['policy'])
+#     policy_model_1 = lambda o, p=p: ray.get(p.call.remote(o))
     
     env = create_env(config=dict(with_video=False, env_name=env_name))
     print("===Created environment...")
     
-    #policy_model_1 = lambda o, pool=pool: list(pool.map(lambda a, v: a.call.remote(v), [o]))[0]
-#     policy_model_1 = model_to_callable(get_policy_value_nets(env_name, 1)['policy'])
-    policy_model_1 = lambda o, p=p: ray.get(p.call.remote(o))
     print("===Created policy...")
     env = MultiAgentToSingleAgent(env_config=dict(env=env, policies={'player_2': policy_model_1}))
     print("===Created MultiToSingle")
