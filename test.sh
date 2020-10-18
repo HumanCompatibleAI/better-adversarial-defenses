8a5353dbdfc3259235bf2ed496232a1a16243a4c
Sergei Volodin <sergei.volodin@epfl.ch>
2020-10-18 02:24:23 +0200

    Add echo statements in test.sh

--- test.sh
+++ test.sh
@@ -14,15 +14,18 @@ conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy
 conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_rllib_env.py
 
 # launching the stable baselines server
+echo "Launching SB server..."
 screen -Sdm "sb_server" conda run -n adv-tf1 python -m frankenstein.stable_baselines_server
 
 # launching training with a few iterations
+echo "Running training..."
 conda run -n adv-tf2 python -m ap_rllib.train --tune external_test
 
 # obtaining the checkpoint
 checkpoint=$(conda run -n adv-tf2 python -m ap_rllib_experiment_analysis.get_last_checkpoint --config external_test)
 
 # making a video
+echo "Making videos..."
 conda run -n adv-tf2 python -m ap_rllib.make_video --config external_test --checkpoint $checkpoint --display $DISPLAY --steps 1
 
 # closing the screen session
