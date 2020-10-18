63f7f2bd920e47bac1b4699e669439f571cd07ed
Sergei Volodin <sergei.volodin@epfl.ch>
2020-10-18 02:33:45 +0200

    Exit on errors in test

--- test.sh
+++ test.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+# exit when any command fails
+set -e
+
 # Disabling GPU
 export CUDA_VISIBLE_DEVICES=-1
 
 # launching mongodb
+echo "Starting mongo"
 sudo service mongodb start
 
 # running pytest
+echo "Testing weights load from pickle files (tf1)"
 conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_load
+echo "Testing weights load from pickle files (tf2)"
 conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_load
+echo "Testing ap_rllib"
 conda run -n adv-tf2 pytest -s -v ap_rllib
+echo "Testing zoo networks consistency"
 conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_prediction
+echo "Testing environment"
 conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_rllib_env.py
 
 # launching the stable baselines server
@@ -29,7 +38,10 @@ echo "Making videos..."
 conda run -n adv-tf2 python -m ap_rllib.make_video --config external_test --checkpoint $checkpoint --display $DISPLAY --steps 1
 
 # closing the screen session
+echo "Stopping sb server"
 screen -S "sb_server" -X kill
 
 # stopping ray
+echo "Stopping ray"
 conda run -n adv-tf2 ray stop
+pkill -f -9 ray
