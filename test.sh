eb9ee711698ec2d4f0b40935a6e23ef394fbd1dd
Sergei Volodin <sergei.volodin@epfl.ch>
2020-11-01 22:11:20 +0100

    Add an option which disables mujoco license usage

--- test.sh
+++ test.sh
@@ -11,7 +11,11 @@ then
 	export DISPLAY=:0
 fi
 
-bash ./mjkey-prompt.sh
+
+if [ "X$1" != "X--no_mujoco_license" ]
+then
+    bash ./mjkey-prompt.sh
+fi
 
 # Disabling GPU
 export CUDA_VISIBLE_DEVICES=-1
@@ -24,31 +28,47 @@ then
 fi
 
 # running pytest
+
+# runs without mujoco
 echo "Testing weights load from pickle files (tf1)"
 conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_load
+
+# runs without mujoco
 echo "Testing weights load from pickle files (tf2)"
 conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_load
+
+# runs without mujoco
 echo "Testing ap_rllib"
 conda run -n adv-tf2 pytest -s -v ap_rllib
-echo "Testing zoo networks consistency"
-conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_prediction
-echo "Testing environment"
-conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_rllib_env.py
+
+if [ "X$1" != "X--no_mujoco_license" ]
+then
+	# does not run without mujoco
+	echo "Testing zoo networks consistency"
+	conda run -n adv-tf1 pytest -s -v gym_compete_rllib/test_load_gym_compete_policy.py::test_prediction
+
+
+	echo "Testing environment"
+	conda run -n adv-tf2 pytest -s -v gym_compete_rllib/test_load_rllib_env.py
+fi
 
 # launching the stable baselines server
 echo "Launching SB server..."
 screen -Sdm "sb_server_test" conda run -n adv-tf1 python -m frankenstein.stable_baselines_server
 
-# launching training with a few iterations
-echo "Running training..."
-conda run -n adv-tf2 python -m ap_rllib.train --tune external_test
+if [ "X$1" != "X--no_mujoco_license" ]
+then
+	# launching training with a few iterations
+	echo "Running training..."
+	conda run -n adv-tf2 python -m ap_rllib.train --tune external_test
 
-# obtaining the checkpoint
-checkpoint=$(conda run -n adv-tf2 python -m ap_rllib_experiment_analysis.get_last_checkpoint --config external_test)
+	# obtaining the checkpoint
+	checkpoint=$(conda run -n adv-tf2 python -m ap_rllib_experiment_analysis.get_last_checkpoint --config external_test)
 
-# making a video
-echo "Making videos..."
-conda run -n adv-tf2 python -m ap_rllib.make_video --config external_test --checkpoint $checkpoint --display $DISPLAY --steps 1
+	# making a video
+	echo "Making videos..."
+	conda run -n adv-tf2 python -m ap_rllib.make_video --config external_test --checkpoint $checkpoint --display $DISPLAY --steps 1
+fi
 
 # closing the screen session
 echo "Stopping sb server"
